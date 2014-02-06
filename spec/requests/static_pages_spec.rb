3b986bc53372aeb83e4782ae97bcdef3d63f9aef
icarus4 <icarus4.chu@gmail.com>
2014-02-06 22:20:41 +0800

    listing: 5.39~5.40

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,16 +20,18 @@ describe "Static pages" do
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { should have_content("Help") }
-    it { should have_title(full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About' }
 
-    it { should have_content('About Us') }
-    it { should have_title(full_title('About')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
@@ -37,5 +39,20 @@ describe "Static pages" do
 
     it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+#    expect(page).to have_title(full_title('Sign Up'))
+#    click_link "sample app"
+#    expect(page).to have_title(full_title('Sample App'))
   end
 end

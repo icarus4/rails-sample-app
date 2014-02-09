f657d343754317b36c5ada84ed2db6354f150af9
icarus4 <icarus4.chu@gmail.com>
2014-02-09 16:51:45 +0800

    listing 6.30: test if email is all lower-case

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,6 +64,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "mIxEdCaSe@eMaIl.CoM" }
+
+    it "should be saved as all lower-case" do
+        @user.email = mixed_case_email
+        @user.save
+        expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   # password
   
   describe "when password is not present" do
